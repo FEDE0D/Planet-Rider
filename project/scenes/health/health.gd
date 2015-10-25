7530c5c6ee287f92270ec1449b1cf87412f43e70
Federico Pacheco <federicogpacheco@gmail.com>
2015-10-25 02:32:45 -0300

    update

--- project/scenes/health/health.gd
+++ project/scenes/health/health.gd
@@ -1,10 +1,16 @@
 
 extends RigidBody2D
 
+var count = 0
+var timer = 0
+
 func _ready():
-	pass
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	timer += delta
 
 func _on_Area2D_body_enter( body ):
-	if body == Globals.get("Player"):
+	if body == Globals.get("Player") and timer > 0.5:
 		Globals.get("Player").pickup_health()
 		queue_free()
